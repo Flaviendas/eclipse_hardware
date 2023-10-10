388b1dc77664ef2c1a8fed777ac0769cb97ddb67
Flavien Da silva <toto@gmail.com>
2023-10-10 14:34:06 +0200

    update projet pc portbale

---
+++ assets/react/controllers/base.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Tata from './tata'
+import Footer from './footer'
+
+export default function sidebar  ()  {
+  return (
+    <div className="base">
+         <Tata />
+        <Footer />
+       
+    </div>
+
+  )
+}
---
+++ assets/react/controllers/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import sidebar from './base'
+{/* <!-- section a votre service  --> */}
+export default function home  ()  {
+    return (
+       
+        <div className="toto"> 
+        <h1 className="title">Eclipse hardware</h1>
+        <p className="info">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates expedita reprehenderit nemo quisquam corrupti perferendis iste! Dolorum quis architecto neque tenetur asperiores consequatur illo excepturi, commodi numquam officiis dignissimos esse.</p>
+        <button className="btn">Explore Now</button>
+        </div>
+)
+}
